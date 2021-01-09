d1ff96c11e30970c46e36751a14d3cfd53b7c4f6
xiong <xiong1422694@163.com>
2021-01-09 16:58:59 +0800

    dataset

---
+++ modelibe.py
@@ -0,0 +1,46 @@
+
+
+class MaskRcnn(object):
+    def __init__(self,mode,config,model_dir):
+        assert mode in ['training', 'inference']
+        self.config = config
+        self.model_dir = model_dir
+        self.set_log_dir()
+        self.keras_model = self.build(mode=mode, config=config)
+
+    def build(self,mode,config):
+        pass
+
+    def set_log_dir(self, model_path=None):
+        """Sets the model log directory and epoch counter.
+
+        model_path: If None, or a format different from what this code uses
+            then set a new log directory and start epochs from 0. Otherwise,
+            extract the log directory and the epoch counter from the file
+            name.
+        """
+        # Set date and epoch counter as if starting a new model
+        self.epoch = 0
+        now = datetime.datetime.now()
+
+        # If we have a model path with date and epochs use them
+        if model_path:
+            # Continue from we left of. Get epoch and date from the file name
+            # A sample model path might look like:
+            # /path/to/logs/coco20171029T2315/mask_rcnn_coco_0001.h5
+            regex = r".*/\w+(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})/mask\_rcnn\_\w+(\d{4})\.h5"
+            m = re.match(regex, model_path)
+            if m:
+                now = datetime.datetime(int(m.group(1)), int(m.group(2)), int(m.group(3)),
+                                        int(m.group(4)), int(m.group(5)))
+                self.epoch = int(m.group(6)) + 1
+
+        # Directory for training logs
+        self.log_dir = os.path.join(self.model_dir, "{}{:%Y%m%dT%H%M}".format(
+            self.config.NAME.lower(), now))
+
+        # Path to save after each epoch. Include placeholders that get filled by Keras.
+        self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
+            self.config.NAME.lower()))
+        self.checkpoint_path = self.checkpoint_path.replace(
+            "*epoch*", "{epoch:04d}")
--- train.py
+++ train.py
@@ -3,16 +3,17 @@ import time
 import numpy as np
 import argparse
 from config import MaskRcnnConfig
-
+import modelibe
 
 class OwnConfig(MaskRcnnConfig):
     """
     需要修改的参数
     """
-    NUM_CLASSES = 23
+    NUM_CLASSES = 23  # 根据自己的训练集类别。包含背景，所以为实际类别+1
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(
         description='Train Mask R-CNN.')
     parser.add_argument('--dataset', required=False,
@@ -22,9 +23,9 @@ if __name__ == '__main__':
                         metavar="/path/to/weights.h5",
                         help="Path to weights .h5 file or 'coco'")
     parser.add_argument('--logs', required=False,
-                        default=DEFAULT_LOGS_DIR,
-                        metavar="/path/to/logs/",
+                        default='logs',
                         help='Logs and checkpoints directory (default=logs/)')
+    prrser.add_argument('--pretrain_path',required=False,default=True)
 
 
     args = parser.parse_args()
@@ -34,3 +35,11 @@ if __name__ == '__main__':
 
     config = OwnConfig()
 
+    model = modelibe.MaskRcnn(mode="training", config=config,
+                              model_dir=args.logs)
+
+    model.load_weights('mask_rcnn_coco.h5', by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
+                                "mrcnn_bbox", "mrcnn_mask"])  # 这个是由coco数据集训练得出的，如果用自己的训练集，只能载入部分
+
+    dataset_train = OwnDataset()
